0de0417c7e59df360060dbcd6393b2b094d0966f
Tomáš Alexa <alexatom@fit.cvut.cz>
2024-12-21 21:06:15 +0100

    plu: Add conform plugin for formatting

---
+++ lua/plugins/format.lua
@@ -0,0 +1,32 @@
+-- Formatter Comform NVIM
+
+-- Lightweight yet powerful formatter plugin for Neovim
+
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "[F]ormat buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      cpp = { "clang-format" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+    },
+    -- Set default options
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = { timeout_ms = 500 },
+  },
+}
