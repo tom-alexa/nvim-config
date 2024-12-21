a3361aa86f666c6c2466e686fe3196077813d717
Tomáš Alexa <alexatom@fit.cvut.cz>
2024-12-21 18:57:17 +0100

    plu: Update which-key and add key mapping groups

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,14 +3,13 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    preset = "modern", -- <classic, modern, helix>
+    spec = {
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>st", group = "[S]earch [T]odos" },
+        { "<leader>h", group = "Git" },
+    }
   },
   keys = {
     {
