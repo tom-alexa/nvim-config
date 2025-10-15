b9426f1495459592989e0373d89d694bb1509de9
Tomáš Alexa <tom.alexa.fin@gmail.com>
2025-10-15 17:30:46 +0200

    plu: Add plugins for markdown rendering

--- init.lua
+++ init.lua
@@ -1,9 +1,7 @@
 -- NVIM Configuration by Tomáš Alexa
 
-
-require 'globals'
-require 'options'
-require 'mappings'
-require 'autocmd'
-require 'config.lazy'
-
+require("globals")
+require("options")
+require("mappings")
+require("autocmd")
+require("config.lazy")
---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" }, -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+  {
+    "tadmccorkle/markdown.nvim",
+    ft = "markdown", -- or 'event = "VeryLazy"'
+    opts = {
+      -- configuration here or empty for defaults
+    },
+  },
+}
